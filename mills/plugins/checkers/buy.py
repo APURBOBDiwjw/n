35aa7b29ac1efed68e8343afa598bd2ce471434c
APURBOBDiwjw <112967120+APURBOBDiwjw@users.noreply.github.com>
2022-09-27 20:35:00 +0000

    Add files via upload

---
+++ mills/plugins/checkers/buy.py
@@ -0,0 +1,43 @@
+"""
+≛ <b>Commands Available</b> ≛
+
+──────────────────────────
+- <code>/buy<code>: Check Available plans for unlocking paid checker gates.
+──────────────────────────
+
+©<a href="https://t.me/Xenpai_ccworld">Xenpai</a>
+"""
+import inspect
+import io
+import json
+import os
+import time
+from fuzzywuzzy.process import extractOne
+from telethon import utils
+# from telethon import Button
+from telethon.tl.custom import Button
+
+from mills import BOT_PIC, client
+from mills.decorators import bot_cmd
+
+
+@bot_cmd(pattern="buy$")
+async def _(m):
+    text = f"""
+
+┌──────────────────────────┐
+    • Premium Plans •
+
+◦ 5$ - Get access to all gates for 28 days.
+◦ 10$ - Get access to all gates. for 70 days
+◦ 20$ - Get access to all gates. for 200 days
+
+○ Payment methods: Crypto, Bank Transfer, Airtm (No Paypal)
+
+└──────────────────────────┘
+"""
+    buttons = [
+        Button.url('Buy Now', 'https://t.me/Xenpai_ccworld'),
+        Button.url('Test Keys', 'https://t.me/Xenpai_ccworld'),
+    ]
+    await m.reply(text,buttons= buttons, file = BOT_PIC)
